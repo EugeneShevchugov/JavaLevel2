fa2a10dfe099750f67823e21ac4dbc66227d771a
@eugene.shevchugov <eugene.shevchugov@gmail.com>
2020-08-06 10:58:36 +0500

    Six Task

---
+++ src/com/company/geekbrains/task6/Server.java
@@ -0,0 +1,44 @@
+package com.company.geekbrains.task6;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    public Server() {
+        Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
+        try(ServerSocket serverSocket = new ServerSocket(8189)) {
+            System.out.println("Сервер запущен, ожидаем подключения...");
+            socket = serverSocket.accept();
+            System.out.println("Клиент подключился : " + socket.getInetAddress());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String msg = scanner.nextLine();
+                        try {
+                            out.writeUTF(msg);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+            while (true) {
+                String str = in.readUTF();
+                if (str.equals("/end")) {
+                    break;
+                }
+                System.out.println("Client: " + str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/company/geekbrains/task6/ServerApp.java
@@ -0,0 +1,7 @@
+package com.company.geekbrains.task6;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
